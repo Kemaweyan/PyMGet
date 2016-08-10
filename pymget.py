ba31136a83a99c266ae69d0be59951739c4f6b1d
Gaidukov Taras <kemaweyan@gmail.com>
2016-08-10 20:38:25 +0300

    Added ConnectionThread and Mirror

--- pymget.py
+++ pymget.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
 import sys, os
-from threading import Thread
+import threading, queue
 from http import client
-from queue import Queue, Empty
 from collections import deque
 
 class Part:
 
-    def __init__(self, thread_name, status, begin, data=None, file_size=None):
-        self.thread_name = thread_name
+    def __init__(self, name, status, begin, data=None, file_size=None):
+        self.name = name
         self.data = data
         self.status = status
         self.begin = begin
@@ -18,46 +17,84 @@ class Part:
 
 
 
-class DownloadThread(Thread):
+class ConnectionThread(threading.Thread):
 
-    user_agent = 'PyMGet/1.00 ({} {}, {})'.format(os.uname().sysname, os.uname().machine, os.uname().release)
-
-    def __init__(self, url, block_size, timeout):
-        Thread.__init__(self)
-        self.name = url
-        self.block_size = block_size
+    def __init__(self, host, timeout):
+        threading.Thread.__init__(self)
         self.timeout = timeout
-        lst = url.split('/', 3)
-        assert lst[0] == 'http:', 'Wrong URL. It is not HTTP protocol.'
-        self.host = lst[2]
-        self.request = '/' + lst[3]
-
-    def start_download(self, position):
-        self.position = position
-        self.start()
+        self.host = host
+        self.ready = threading.Event()
 
     def run(self):
-        headers = {'User-Agent': self.user_agent, 'Range': 'bytes={}-{}'.format(self.position, self.position + self.block_size - 1)}
-        conn = client.HTTPConnection(self.host, timeout=self.timeout)
-        conn.request('GET', self.request, headers=headers)
-        response = conn.getresponse()
+        self.conn = client.HTTPConnection(self.host, timeout=self.timeout)
+        self.ready.set()
 
-        if response.status == 206:
+
+
+
+class DownloadThread(threading.Thread):
+
+    user_agent = 'PyMGet/1.01 ({} {}, {})'.format(os.uname().sysname, os.uname().machine, os.uname().release)
+
+    def __init__(self, name, conn, request, begin, block_size):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.conn = conn
+        self.request = request
+        self.begin = begin
+        self.block_size = block_size
+
+    def run(self):
+        headers = {'User-Agent': self.user_agent, 'Range': 'bytes={}-{}'.format(self.begin, self.begin + self.block_size - 1)}
+        try:
+            self.conn.request('GET', self.request, headers=headers)
+            response = self.conn.getresponse()
+            if response.status != 206:
+                raise client.HTTPException
             file_size = int(response.getheader('Content-Range').split('/')[-1])
             data = response.read()
-            part = Part(self.name, response.status, self.position, data, file_size)
-        else:
-            part = Part(self.name, response.status, self.position)
+            part = Part(self.name, response.status, self.begin, data, file_size)
+            response.close()
+        except:
+            part = Part(self.name, 0, self.begin)
+        finally:
+            Manager.data_queue.put(part)
 
-        Manager.data_queue.put(part)
-        
 
+
+
+class Mirror:
+
+    def __init__(self, url, block_size, timeout):
+        self.url = url
+        self.block_size = block_size
+        self.timeout = timeout
+        url_parts = self.url.split('/', 3)
+        assert url_parts[0] == 'http:', 'Wrong URL. It is not HTTP protocol.'
+        self.host = url_parts[2]
+        self.request = '/' + url_parts[3]
+        self.thread = ConnectionThread(self.host, self.timeout)
+        self.thread.start()
+
+    def wait_connection(self):
+        if not self.thread.ready.wait(0.1):
+            return False
+        self.thread.join()
+        self.conn = self.thread.conn
+        return True
+
+    def download(self, begin):
+        self.thread = DownloadThread(self.url, self.conn, self.request, begin, self.block_size)
+        self.thread.start()
+
+    def close(self):
+        self.conn.close()
 
 
 
 class Manager:
 
-    data_queue = Queue()
+    data_queue = queue.Queue()
 
     def __init__(self, urls, block_size, filename, timeout):
         if filename is None:
@@ -69,25 +106,32 @@ class Manager:
 
         self.block_size = block_size
 
-        self.timeout = timeout
-
-        self.threads = {}
+        self.mirrors = {}
         for url in urls:
             try:
-                self.threads[url] = DownloadThread(url, self.block_size, timeout)
+                self.mirrors[url] = Mirror(url, self.block_size, timeout)
             except AssertionError as e:
                 print(str(e))
 
-
     def download(self):
-        position = 0
+        begin = 0
         file_size = 0
         written_bytes = 0
         failed_parts = deque([])
+        wait_mirrors = self.mirrors.copy()
+
+        while len(wait_mirrors) > 0:
+            remain_mirrors = {}
+            for name, mirror in wait_mirrors.items():
+                if not mirror.wait_connection():
+                    print(name)
+                    remain_mirrors[name] = wait_mirrors[name]
+            wait_mirrors = remain_mirrors
+
         with open(self.filename, 'wb') as outfile:
-            for thread in self.threads.values():
-                thread.start_download(position)
-                position += self.block_size
+            for mirror in self.mirrors.values():
+                mirror.download(begin)
+                begin += self.block_size
 
             while file_size == 0 or written_bytes < file_size:
                 part = self.data_queue.get()
@@ -102,22 +146,27 @@ class Manager:
                     outfile.seek(part.begin, 0)
                     written_bytes += outfile.write(part.data)
 
-                    self.threads[part.thread_name].join()
-                    thread = DownloadThread(part.thread_name, self.block_size, self.timeout)
-                    self.threads[part.thread_name] = thread
+                    mirror = self.mirrors[part.name]
+                    mirror.thread.join()
 
                     if len(failed_parts) > 0:
-                        thread.start_download(failed_parts.popleft())
-                    elif position < file_size:
-                        thread.start_download(position)
-                        position += self.block_size
+                        mirror.download(failed_parts.popleft())
+                    elif begin < file_size:
+                        mirror.download(begin)
+                        begin += self.block_size
                 except AssertionError as e:
                     print(e)
                     failed_parts.append(part.begin)
-                    self.threads[part.thread_name].join()
-                    del self.threads[part.thread_name]
-                    if len(self.threads) == 0:
+                    self.mirrors[part.name].thread.join()
+                    self.mirrors[part.name].close()
+                    del self.mirrors[part.name]
+                    if len(self.mirrors) == 0:
                         return 1
+
+            for mirror in self.mirrors.values():
+                mirror.thread.join()
+                mirror.close()
+
         return 0
 
 
