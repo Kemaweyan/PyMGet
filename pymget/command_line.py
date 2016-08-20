50ebf4377a134cdc84e783a227df073ac6ed11d9
Gaidukov Taras <kemaweyan@gmail.com>
2016-08-20 20:54:44 +0300

    Translated comments into English

--- pymget/command_line.py
+++ pymget/command_line.py
@@ -9,103 +9,102 @@ from pymget.console import Console
 from pymget.messages import Messages
 from pymget.errors import CommandLineError
 
-# Класс парсера командной строки
-
 class CommandLine:
 
     """
-    Выбирает параметры командной строки и выполянет соответствующие действия.
+    Reads arguments in the command line and
+    executes required actions.
 
     """
     def __init__(self, argv):
 
         """
-        :argv: список параметров, тип sequence (str)
+        :argv: a sequence of argument, elements type str
 
         """
         self.lang = Messages()
-        self.argv = argv[1:] # первый параметр отбрасываем (содержит имя программы)
-        self.block_size = 4 * 2**20 # размер блока по-молчанию - 4МБ
-        self.filename = '' # имя файла неизвестно
-        self.timeout = 10 # таймаут по-умолчанию - 10 сек
-        self.urls = [] # список зеркал пуст
-        # шаблон для нахождения URL
+        self.argv = argv[1:] # the first argument is a name of program, pass it
+        self.block_size = 4 * 2**20 # default block size is 4MB
+        self.filename = '' # filename is unknown
+        self.timeout = 10 # default timeout is 10 seconds
+        self.urls = [] # the list of mirros is empty
+        # pattern to search URLs
         self.url_re = re.compile('^(?:https?|ftp)://(?:[\w\.-]+(?::\d+)?)/')
         self.console = Console()
 
     def show_help(self):
 
         """
-        Выводит текст помощи и завершает программу.
+        Shows the help message and terminates program.
 
         """
         import __main__
+        # use __main__ module to define real program name 
         self.console.out(textwrap.dedent(self.lang.message.help.format(os.path.basename(__main__.__file__))))
         sys.exit()
 
     def parse_block_size(self, block_size):
 
         """
-        Парсит параметр размера блока.
+        Parses an argument of block size.
 
-        :block_size: значение параметра, тип str
+        :block_size: value of argument, type str
 
         """
-        bs_re = re.compile('(\d+)(\w)?') # шаблон параметра "число + (опционально) буква"
+        bs_re = re.compile('(\d+)(\w)?') # pattern for argument "number + (optional) "char"
         matches = bs_re.match(block_size)
-        if not matches: # если параметр не сообтветствует шаблону
-            # ошибка командной строки
+        if not matches: # argument does not mutch - wrong argument
             raise CommandLineError(self.lang.error.wrong_argument + self.lang.error.wrong_param.format('block size', block_size))
-        self.block_size = int(matches.group(1)) # присваиваем размеру блока числовуч часть параметра
-        if matches.group(2): # если указана размерность
-            if matches.group(2) in 'kK': # если k или K
-                self.block_size *= 2**10 # значит в килобайты
-            elif matches.group(2) in 'mM': # если m или M
-                self.block_size *= 2**20 # значит мегабайты
+        self.block_size = int(matches.group(1)) # assign to block size a value of number
+        if matches.group(2): # there is a char in the parameter
+            if matches.group(2) in 'kK': # k or K
+                self.block_size *= 2**10 # that's kilobytes
+            elif matches.group(2) in 'mM': # m or M
+                self.block_size *= 2**20 # that's megabytes
             else:
-                # иначе - ошибка командной строки
+                # not m, M, k or K - wrong argument
                 raise CommandLineError(self.lang.error.wrong_argument + self.lang.error.wrong_param.format('block size', block_size))
 
     def parse_timeout(self, timeout):
 
         """
-        Парсит параметр времени ожидания.
+        Parses an argument of timeout
 
-        :timeout: значение параметра, тип str
+        :timeout: value of argument, type str
 
         """
-        if not timeout.isdigit(): # если параметр не число
-            # ошибка командной строки
+        if not timeout.isdigit(): # parameter is not a number - wrong argument
             raise CommandLineError(self.lang.error.wrong_argument + self.lang.error.wrong_param.format('timeout', timeout))
-        self.timeout = int(timeout) # присваиваем таймаут
+        self.timeout = int(timeout) # assign timeout
 
     def parse_urls_file(self, urls_file):
 
         """
-        Парсит параметр файла со списком ссылок.
+        Parses a file with a list of URLs
 
-        :urls_file: значение параметра (имя файла), тип str
+        :urls_file: value of parameter (filename), type str
 
         """
         try:
             urls = []
-            with open(urls_file, 'r') as links: # пробуем открыть текстовый файл
-                for link in links: # каждую строку из файла
-                    urls.append(link.strip('\r\n')) # добавляем в список, отбросив символы перевода строки
-            self.urls.extend(urls) # дополняем список ссылок
-        except FileNotFoundError: # файл не найден
+            with open(urls_file, 'r') as links: # try to open the file in text mode
+                # add each line to list without newline symbols
+                for link in links:
+                    urls.append(link.strip('\r\n'))
+            self.urls.extend(urls) # add these URLs to the list of URLs from command line
+        except FileNotFoundError:
             raise CommandLineError(self.lang.error.file_not_found.format(urls_file))
-        except PermissionError: # недостаточно прав
+        except PermissionError:
             raise CommandLineError(self.lang.error.links_permission_denied.format(urls_file))
-        except UnicodeDecodeError: # неверная структура файла
+        except UnicodeDecodeError: # specified file is not a correct text file
             raise CommandLineError(self.lang.error.corrupted_file.format(urls_file))
      
     def parse_out_file(self, filename):
 
         """
-        Парсит параметр имени выходного файла.
+        Parses a name of output file
 
-        :filename: значение параметра, тип str
+        :filename: value of parameter, type str
 
         """
         self.filename = filename
@@ -113,58 +112,58 @@ class CommandLine:
     def parse_long_arg(self, arg):
 
         """
-        Парсит параметры, переданные в длинном формате --arg=value
+        Parses arguments in long format --arg=value
 
-        :arg: параметр, тип str
+        :arg: an argument, type str
 
         """
-        name, param = arg.split('=') # размеляем по символу =
-        return param # возвращаем значение
+        name, param = arg.split('=') # split by =
+        return param # return the second part
 
     def parse(self):
 
         """
-        Парсит командную строку.
+        Parses the command line
 
         """
-        args_iterator = iter(self.argv) # создадим итератор
-        # для доступа к слудющим элементам с помощью next
+        args_iterator = iter(self.argv) # create an iterator
+        # to have access to next() to get parameters of arguments
         for arg in args_iterator:
             if arg == '-h' or arg == '--help':
-                self.show_help() # показываем помощь
+                self.show_help() # show the help message
             if arg == '-v' or arg == '--version':
-                sys.exit() # информация о версии уже показана, просто выходим
+                sys.exit() # the version info already shown, just exit
             elif arg == '-b':
-                # парсим размер блока, передав ему следующий элемент
+                # parse block size, pass next item to the method
                 self.parse_block_size(next(args_iterator))
             elif arg == '-T':
-                # парсим время ожидания, передав ему следующий элемент
+                # parse timeout, pass next item to the method
                 self.parse_timeout(next(args_iterator))
             elif arg == '-u':
-                # парсим файл со списком ссылок, передав ему следующий элемент
+                # parse URLs file, pass next item to the method
                 self.parse_urls_file(next(args_iterator))
             elif arg == '-o':
-                # парсим имя выходного файла, передав ему следующий элемент
+                # parse the name of outfile, pass next item to the method
                 self.parse_out_file(next(args_iterator))
             elif arg.startswith('--block-size='):
-                # парсим размер блока, передав ему длинный параметр
+                # parse block size, get parameter from long argument
                 self.parse_block_size(self.parse_long_arg(arg))
             elif arg.startswith('--timeout='):
-                # парсим время ожидания, передав ему длинный параметр
+                # parse timeout, get parameter from long argument
                 self.parse_timeout(self.parse_long_arg(arg))
             elif arg.startswith('--urls-file='):
-                # парсим файл со списком ссылок, передав ему длинный параметр
+                # parse URLs file, get parameter from long argument
                 self.parse_urls_file(self.parse_long_arg(arg))
             elif arg.startswith('--out-file='):
-                # парсим имя выходного файла, передав ему длинный параметр
+                # parse the name of outfile, get parameter from long argument
                 self.parse_out_file(self.parse_long_arg(arg))
-            elif self.url_re.match(arg): # параметр соответствует шаблону URL
-                self.urls.append(arg) # добавляем его в список URL
+            elif self.url_re.match(arg): # argument matches the URL pattern
+                self.urls.append(arg) # add it to the list of URLs
             else:
-                # если параметр не соответствует ничему вышеперечисленному
-                # выводим предупреждение про неизвестный аргумент и пропускаем его
+                # argument does not match anything known
+                # show warning and pass the argument
                 self.console.warning(self.lang.warning.unknown_arg.format(arg))
 
-        # создадим из ссылок объекты URL,
-        # предварительно отфильтровав их по шаблону
+        # create URL objects from the links,
+        # previously filter them with the URL pattern
         self.urls = map(lambda url: URL(url), filter(lambda url: self.url_re.match(url), self.urls))
