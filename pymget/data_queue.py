cbea85d32eaf1c19df6010dee5e37e0fcdbb6840
Gaidukov Taras <kemaweyan@gmail.com>
2016-08-20 22:33:38 +0300

    Added comments

--- pymget/data_queue.py
+++ pymget/data_queue.py
@@ -7,5 +7,15 @@ from pymget.utils import singleton
 
 @singleton
 class DataQueue(queue.Queue):
+
+    """
+    Queue of TaskInfo objects produced
+    by network threads and used by Manager.
+    Complete implementation is in queue.Queue
+    class, but this class makes it singleton
+    to access to single object from any place
+    of the project.
+
+    """
     pass
 
